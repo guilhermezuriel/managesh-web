a303bcce906aef44d3cfd63bb45f3e5e9a689113
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-19 16:06:51 -0300

    Adding layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Login } from './pages/auth/login'
 
 export const router = createBrowserRouter([
   {
-    path: '/dashboard',
-    element: <Dashboard />,
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
   },
 ])
