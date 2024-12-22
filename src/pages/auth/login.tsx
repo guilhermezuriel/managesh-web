276b8f603c88e8c9df8abbd97a634a308e87da7d
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-22 16:54:55 -0300

    installing components from shadcnui

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -3,7 +3,17 @@ import { Helmet } from 'react-helmet-async'
 export const Login = () => {
   return (
     <div>
-      <Helmet title="Login" /> login
+      <Helmet title="Login" />
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">Acompanhe suas vendas</p>
+        </div>
+
+        <form className="flex flex-col"></form>
+      </div>
     </div>
   )
 }
