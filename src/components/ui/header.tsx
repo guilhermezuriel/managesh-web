d555420ea599beb2018d2388b0f0244602c124b0
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-29 23:33:16 -0300

    Creation of app header

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,25 @@
+import { Separator } from '@radix-ui/react-separator'
+import { ClipboardPenLine, Coffee, Home } from 'lucide-react'
+
+import { NavLink } from './nav-link'
+
+export const Header = () => {
+  return (
+    <header className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Coffee className="h-6 w-6" />
+        <Separator orientation="vertical" className="h-6" />
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Home
+          </NavLink>
+          <NavLink to="/orders">
+            <ClipboardPenLine className="h-4 w-4" />
+            Orders
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
+}
