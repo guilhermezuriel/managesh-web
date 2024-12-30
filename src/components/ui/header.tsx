57420a64f1fe6a01fb7529e18bbc9e04450a9e04
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-30 14:24:48 -0300

    User dropdown menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,6 +2,7 @@ import { Separator } from '@radix-ui/react-separator'
 import { ClipboardPenLine, Coffee, Home } from 'lucide-react'
 
 import { ThemeToggle } from '../theme/theme-toggle'
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
 
 export const Header = () => {
@@ -22,6 +23,7 @@ export const Header = () => {
         </nav>
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
+          <AccountMenu />
         </div>
       </div>
     </header>
