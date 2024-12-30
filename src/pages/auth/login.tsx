cf2f667bbb6ad99a4409b11f4b3419866dd4d629
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-29 22:30:02 -0300

    Including button to acess to subscribe or login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -28,26 +29,31 @@ export const Login = () => {
     }
   }
   return (
-    <div>
+    <>
       <Helmet title="Login" />
-      <div className="flex w-[350px] flex-col justify-center gap-6">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Acess dashboard
-          </h1>
-          <p className="text-sm text-muted-foreground">Track your sales!</p>
-        </div>
-
-        <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
-          <div className="space-y-2">
-            <Label htmlFor="email">email:</Label>
-            <Input id="email" type="email" {...register('email')} />
+      <div className="p-8">
+        <Button asChild variant="ghost" className="absolute right-8 top-8">
+          <Link to="/subscribe">New store</Link>
+        </Button>
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acess dashboard
+            </h1>
+            <p className="text-sm text-muted-foreground">Track your sales!</p>
           </div>
-          <Button disabled={isSubmitting} className="w-full">
-            Login
-          </Button>
-        </form>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">email:</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <Button disabled={isSubmitting} className="w-full">
+              Login
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/auth/subscribe.tsx
+++ src/pages/auth/subscribe.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -31,53 +32,62 @@ export const Subscribe = () => {
     }
   }
   return (
-    <div>
+    <>
       <Helmet title="Subscribe" />
-      <div className="flex w-[350px] flex-col justify-center gap-6">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Create a free account
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            Subscripe your store !
-          </p>
-        </div>
+      <div className="p-8">
+        <Button asChild variant="ghost" className="absolute right-8 top-8">
+          <Link to="/login">Login</Link>
+        </Button>
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create a free account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Subscripe your store !
+            </p>
+          </div>
 
-        <form className="space-y-4" onSubmit={handleSubmit(handleSubscribe)}>
-          <div className="space-y-2">
-            <Label htmlFor="storeName">Your store name</Label>
-            <Input id="storeName" type="text" {...register('storeName')} />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="managerName">Your name</Label>
-            <Input id="managerName" type="text" {...register('managerName')} />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="phone">Your email</Label>
-            <Input id="email" type="email" {...register('email')} />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="phone">Your phone</Label>
-            <Input id="phone" type="text" {...register('phone')} />
-          </div>
-          <div className="flex items-center gap-2">
-            <input type="checkbox" />
-            <span>
-              Agreed with{' '}
-              <u>
-                <a href="">terms</a>
-              </u>{' '}
-              and{' '}
-              <u>
-                <a href="">privacy</a>
-              </u>
-            </span>
-          </div>
-          <Button disabled={isSubmitting} className="w-full">
-            Subscribe
-          </Button>
-        </form>
+          <form className="space-y-4" onSubmit={handleSubmit(handleSubscribe)}>
+            <div className="space-y-2">
+              <Label htmlFor="storeName">Your store name</Label>
+              <Input id="storeName" type="text" {...register('storeName')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="managerName">Your name</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register('managerName')}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Your email</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Your phone</Label>
+              <Input id="phone" type="text" {...register('phone')} />
+            </div>
+            <div className="flex items-center gap-2">
+              <input type="checkbox" />
+              <span>
+                Agreed with{' '}
+                <u>
+                  <a href="">terms</a>
+                </u>{' '}
+                and{' '}
+                <u>
+                  <a href="">privacy</a>
+                </u>
+              </span>
+            </div>
+            <Button disabled={isSubmitting} className="w-full">
+              Subscribe
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
