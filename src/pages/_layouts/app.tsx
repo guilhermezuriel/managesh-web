799ebb37963266ae88114a36014f1f73baa295c5
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-01-07 19:17:58 -0300

    Adding margin to the app layout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -6,7 +6,7 @@ export const AppLayout = () => {
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
-      <div className="p8 flex flex-1 flex-col gap-4 pt-6">
+      <div className="p8 ml-5 mr-5 flex flex-1 flex-col gap-4 pt-6">
         <Outlet />
       </div>
     </div>
