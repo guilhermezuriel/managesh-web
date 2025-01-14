472073b01d8a596130f948e97e1671281a032016
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-01-14 11:37:34 -0300

    Page errors and not found

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,18 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { Login } from './pages/auth/login'
 import { Subscribe } from './pages/auth/subscribe'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -35,5 +38,9 @@ export const router = createBrowserRouter([
         element: <Subscribe />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
